b32a21a4c6f33d737994bcc279023d0430392dcf
nurrizatulmufidah <nurrizatulmufidah610@gmail.com>
2025-07-01 11:45:58 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+const pengurus = [
+  { nama: "Nuris Syamsi", jabatan: "Ketua", alamat: "Jakarta" },
+  { nama: "Dewi Rahayu", jabatan: "Sekretaris", alamat: "Bandung" },
+  { nama: "Andi Setiawan", jabatan: "Bendahara", alamat: "Surabaya" }
+];
